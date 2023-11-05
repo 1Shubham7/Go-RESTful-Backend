20b2e05142d89ae0fb2a01392e5a93c2bd0ae65a
Shubham Singh <shubhammahar1306@gmail.com>
2023-11-05 09:55:24 +0530

    add: created a gin web server

--- models/UserModel.go
+++ models/UserModel.go
@@ -1,6 +1,9 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time
+)"
 
 type User struct{
 	ID primitive.ObjectID	`bson:"id"`
