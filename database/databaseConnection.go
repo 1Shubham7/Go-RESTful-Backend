ee71efb7201d89df7d9d715bac5ae6ee5fdf534d
Shubham Singh <shubhammahar1306@gmail.com>
2023-11-06 21:26:25 +0530

    add: working on database connection, added .env file

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -1,1 +1,11 @@
 package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+	"conetxt"
+	"github.com/joho/godotenv"
+	""
+)
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import(
+	"github.com/gin-gonic/gin"
+	"os"
+	routes "github.com/1shubham7/jwt/routes"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.AuthRoutes(router)
+	routes.UserRoutes(router)
+
+	// creating two apis
+
+	router.GET("/api-1", func(c *gin.Context){
+		c.JSON(200, gin.H{"success":"Access granted for api-1"})
+	})
+
+	router.GET("api-2", func(c *gin.Context){
+		c.JSON(200, gin.H{"success":"Access granted for api-2"})
+	})
+
+	router.Run(":" + port)
+}
