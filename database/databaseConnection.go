6e1212b55d3b5281f12cb736d54b04c29e720ae7
Shubham Singh <shubhammahar1306@gmail.com>
2023-11-06 21:42:26 +0530

    add: setting up .env file

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -5,7 +5,17 @@ import (
 	"log"
 	"os"
 	"time"
-	"conetxt"
+	"context"
 	"github.com/joho/godotenv"
-	""
+	"go.mongodb.org/mongo-driver/mongo"
+	"go/mongodb.org/mongo-driver/mongo/options"
 )
+
+func DBinstance() *mongo.Client{
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error locading the .env file")
+	}
+
+	MongoDb := os.Getenv("THE_MONGODB_URL")
+}
