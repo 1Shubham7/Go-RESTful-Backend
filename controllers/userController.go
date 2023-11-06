50e583e508e0a8a984142db641b01ad4f40998a3
Shubham Singh <shubhammahar1306@gmail.com>
2023-11-07 00:33:03 +0530

    add: working on userColnroller, unserCollection created

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,8 +1,22 @@
 package controllers
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
 
+	database "github.com/1shubham7/jwt/database"
+	helper "github.com/1shubham7/jwt/helpers"
+	models "github.com/1shubham7/jwt/models"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"golang.org/x/crypto/bcrypt"
 )
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
 
 func Hashpassword()
 
@@ -13,3 +27,7 @@ func SignUp() {
 }
 
 func Login()
+
+func GetUsers()
+
+func GetUserById()
