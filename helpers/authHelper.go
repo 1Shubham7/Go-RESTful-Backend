01abc607f498263c388ac5bd30ee33081412f0cc
Shubham Singh <shubhammahar1306@gmail.com>
2023-11-12 11:54:32 +0530

    add: added the MatchUserTypeToUserId func

--- helpers/authHelper.go
+++ helpers/authHelper.go
@@ -5,6 +5,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func MatchUserTypeToUserId() {
+func MatchUserTypeToUserId(c *gin.Context, userId string) (err error) {
+	//  This is the match user function
+	userType := c.GetString("user_type")
+	uid := c.GetString("uid")
+	err = nil
+
+	// this means that user is USER not ADMIN and uid is not of the user. Because user can only access his id,
+	// admin can access anyone's id
+	if userId == "USER" && uid != userId {
+		err = errors.New("You are not authorized to access this user")
+	}
+	err = CheckUserType(c, userType)
+	return err
 
 }
