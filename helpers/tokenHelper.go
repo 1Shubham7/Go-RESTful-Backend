dd9bb857bcae3798d3c5397393aa33905d549753
Shubham Singh <shubhammahar1306@gmail.com>
2023-11-14 04:54:24 +0530

    add: added a SignedDetails datatype, working on generateallTokens()

--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -6,14 +6,31 @@ import (
 	"log"
 	"os"
 	"time"
+
+	"github.com/1shubham7/jwt/database"
 	"github.com/akhil/golang-jwt-project/database"
-	jwt "github.com/dgrijalva/jwt-go"  // golang driver for jwt
+	jwt "github.com/dgrijalva/jwt-go" // golang driver for jwt
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func GenerateAllTokens(){
+type SignedDetails struct {
+	Email string
+	First_name string
+	Last_name string
+	Uid string
+	User_type string
+	jwt.StandardClaims
+}
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+
+// btw we sould have our secret key in .env for production 
+var SECRET_KEY string = os.Getenv("SECRET_KEY")
+
+func GenerateAllTokens(email string, firstName string, ){
+
 
 }
