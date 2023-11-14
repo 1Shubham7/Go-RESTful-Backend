0a05698dd5042e1e8aefd77fd7f0ea93b3a0de20
Shubham Singh <shubhammahar1306@gmail.com>
2023-11-14 08:21:29 +0530

    add: GenerateAllTokens() ready

--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -50,4 +50,13 @@ func GenerateAllTokens(email string, firstName string, lastName string, userType
 				ExpiresAt: time.Now().Local().Add(time.Hour *time.Duration(172)).Unix(),
 		},
 	}
+
+	token ,err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(SECRET_KEY))
+	refreshToken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims).SignedString([]byte(SECRET_KEY))
+
+	if err!= nil{
+		log.Panic(err)
+		return
+	}
+	return token, refreshToken, err
 }
